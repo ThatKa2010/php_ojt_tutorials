aca157d2f391f2477998401328d23212bccc5b4b
ThatKa2010 <thatka920@gmail.com>
2024-03-06 20:42:14 +0630

    makeDiamondShape_tutorial-02_06_04_2024

---
+++ Tutorial-02/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Diamond Shape</title>
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h1>Diamond Shape</h1>
+<div class="diamond">
+  <pre>
+<?php
+function makeDiamondShape($row) 
+{
+    if (!is_numeric($row)) {
+        echo '$row parameter must be a number';
+        return;
+    }
+
+    if ($row % 2 == 0) {
+        echo '$row parameter must be an odd number';
+        return;
+    }
+
+    $midpoint = ($row - 1) / 2;
+
+    // Upper half of the diamond
+    for ($i = 0; $i <= $midpoint; $i++) {
+        echo str_repeat(' ', $midpoint - $i); 
+        echo str_repeat('*', 2 * $i + 1); 
+        echo "<br>"; 
+    }
+
+    // Lower half of the diamond
+    for ($i = $midpoint - 1; $i >= 0; $i--) {
+        echo str_repeat(' ', $midpoint - $i); 
+        echo str_repeat('*', 2 * $i + 1); 
+        echo "<br>";
+    }
+}
+
+
+makeDiamondShape(1); 
+?>
+</pre>
+</div>
+</body>
+</html>
