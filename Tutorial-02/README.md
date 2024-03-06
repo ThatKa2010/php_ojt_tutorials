2fbb557ee7833c338a2ad137e68f3b70a6d84bd9
ThatKa2010 <thatka920@gmail.com>
2024-03-06 20:50:39 +0630

    make_DiamondShape&validation_06_04_2024

---
+++ Tutorial-02/README.md
@@ -0,0 +1,18 @@
+# Tutorial 01
+
+## Make the diamond shape and check Validation.
+
+## Folder Structure
+
+```
+.
+├── ccs/
+│   ├── reset.css
+│   └── style.css
+├── demo/
+│   └── preview.png
+├── index.php
+└── README.md
+```
+
+![preview.png](demo/preview.png)
--- Tutorial-02/index.php
+++ Tutorial-02/index.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Diamond Shape</title>
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diamond Shape</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-  <h1>Diamond Shape</h1>
-<div class="diamond">
-  <pre>
+    <h1>Diamond Shape</h1>
+    <div class="diamond">
+    <pre>   
 <?php
-function makeDiamondShape($row) 
+/**
+        * Summary of makeDiamondShape
+        * @param mixed $row
+        * @return void
+        */
+function makeDiamondShape($row)
 {
     if (!is_numeric($row)) {
         echo '$row parameter must be a number';
@@ -28,23 +35,24 @@ function makeDiamondShape($row)
 
     // Upper half of the diamond
     for ($i = 0; $i <= $midpoint; $i++) {
-        echo str_repeat(' ', $midpoint - $i); 
-        echo str_repeat('*', 2 * $i + 1); 
-        echo "<br>"; 
+        echo str_repeat(' ', $midpoint - $i);
+        echo str_repeat('*', 2 * $i + 1);
+        echo "<br>";
     }
 
     // Lower half of the diamond
     for ($i = $midpoint - 1; $i >= 0; $i--) {
-        echo str_repeat(' ', $midpoint - $i); 
-        echo str_repeat('*', 2 * $i + 1); 
+        echo str_repeat(' ', $midpoint - $i);
+        echo str_repeat('*', 2 * $i + 1);
         echo "<br>";
     }
 }
 
 
-makeDiamondShape(1); 
+makeDiamondShape(9);
 ?>
 </pre>
-</div>
+    </div>
 </body>
+
 </html>
