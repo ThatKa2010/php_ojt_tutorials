b0e0f76556eae6af71e05bc38981fefc09a5d44a
ThatKa2010 <thatka920@gmail.com>
2024-03-15 18:15:32 +0630

    change_PHPversion8.1.10_tutorial-07_15_03_2024

--- Tutorial_07/index.php
+++ Tutorial_07/index.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
     <div class="container">
         <div class="ttl">
@@ -18,10 +17,10 @@
         <form action="generate.php" method="post">
             <label for="qr">QR Name</label>
             <input type="text" name="qrname" id="qr" placeholder="Enter QR name:">
-            <?php if (isset($_GET['error'])) {
+            <?php if (isset ($_GET['error'])) {
                 echo "<span>" . $_GET['error'] . "</span>";
             }
-            if (isset($_GET['error-exists'])) {
+            if (isset ($_GET['error-exists'])) {
                 echo "<span>" . $_GET['error-exists'] . "</span>";
             }
             ?>
@@ -31,31 +30,34 @@
 
     <div class="current-qr">
         <?php
-        if (isset($_GET['success'])) {
+        if (isset ($_GET['success'])) {
             $qrname = $_GET['qrname'];
             $qrImagePath = 'images/' . $qrname . '.png';
             echo '<h2>QR code is successfully generated.</h2>';
-            echo '<img src="' . $qrImagePath . '" alt="QR Code">';
+            echo '<img src="' . $qrImagePath . '" alt="QR Code" style="width:300px;">';
         }
         ?>
     </div>
 
-    <div class="img-con">
-        <?php
-        $directory = 'images/';
+    <div class="show-qr">
+        <h3>QR Lists</h3>
+        <div class="img-con">
+            <?php
+            $directory = 'images/';
 
-        // Create RecursiveDirectoryIterator
-        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
+            // Create RecursiveDirectoryIterator
+            $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
 
-        foreach ($iterator as $file) {
-            if ($file->isFile() && in_array(strtolower($file->getExtension()), ['png'])) {
-                echo '<div class="image">';
-                echo '<img src="' . $file->getPathname() . '" alt="Uploaded Image"><br>';
-                echo '<p>' . basename($file->getPathname()) . '</p>';
-                echo '</div>';
+            foreach ($iterator as $file) {
+                if ($file->isFile() && in_array(strtolower($file->getExtension()), ['png'])) {
+                    echo '<div class="image">';
+                    echo '<img src="' . $file->getPathname() . '" alt="Uploaded Image"><br>';
+                    echo '<p>' . basename($file->getPathname()) . '</p>';
+                    echo '</div>';
+                }
             }
-        }
-        ?>
+            ?>
+        </div>
     </div>
 </body>
 </html>
